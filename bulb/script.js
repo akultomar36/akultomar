efea2b92d6144b3d6306475828332893d83c2d4f
akultomar36 <92429082+akultomar36@users.noreply.github.com>
2022-04-12 21:06:36 +0530

    bulb
    assignment bulb

---
+++ bulb/script.js
@@ -0,0 +1,9 @@
+function bulbOn(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src" , "https://www.w3schools.com/js/pic_bulbon.gif");
+}
+
+function bulbOff(){
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src" , " https://www.w3schools.com/js/pic_bulboff.gif");
+}
